3a97f99dc0138775e1bd7cd768471d961d66bf01
elsa sabilah <elsabilah1@gmail.com>
2023-01-03 08:50:30 +0700

    setup tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='grid place-items-center h-[100vh]'>
+      <h1 className='text-3xl font-bold'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
